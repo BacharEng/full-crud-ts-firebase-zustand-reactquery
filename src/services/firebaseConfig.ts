fd4cac7537d19fda971406887bfb3e956f50b394
Sagi Bachar Adler <sagienator@gmail.com>
2024-03-16 14:11:27 +0200

    Update firebaseConfig.ts

--- src/services/firebaseConfig.ts
+++ src/services/firebaseConfig.ts
@@ -1,16 +1,14 @@
-
 import { initializeApp, FirebaseApp } from "firebase/app";
-import { getFirestore, Firestore } from 'firebase/firestore'
+import { getFirestore, Firestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "AUTH_DOMAIN",
-  projectId: "PROJECT_ID",
-  storageBucket: "STORAGE_BUCKET",
-  messagingSenderId: "MESSAGE_SENDER_ID",
-  appId: "APP_ID"
+  apiKey: "AIzaSyAKlzUk4frigeA3jBgOu457oHSqCeRv3nQ",
+  authDomain: "cheapflightsapp-dc6bb.firebaseapp.com",
+  projectId: "cheapflightsapp-dc6bb",
+  storageBucket: "cheapflightsapp-dc6bb.appspot.com",
+  messagingSenderId: "293754397179",
+  appId: "1:293754397179:web:9865f72b51edeeb62c6b5e",
 };
-
 
 const app: FirebaseApp = initializeApp(firebaseConfig);
 export const db: Firestore = getFirestore(app);
